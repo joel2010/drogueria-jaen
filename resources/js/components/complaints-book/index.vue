f4ff6b45e61cb258b080f9c3d87d219dcf75f0a7
Jose Gonzales <josegp0901@hotmail.com>
2025-10-06 10:13:36 -0500

    Update index.vue

--- resources/js/components/complaints-book/index.vue
+++ resources/js/components/complaints-book/index.vue
@@ -48,7 +48,7 @@
                     <div>
                         <label for="clientType" class="block text-subtitle">TIPO DE CLIENTE</label>
                         <select id="clientType" v-model="formData.clientType"
-                            class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md shadow-sm sm:max-w-[50%]">
+                            class="mt-1 block w-full pl-3 pr-10 py-2 text-base border border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md shadow-sm sm:max-w-[50%]">
                             <option value="" disabled>Seleccione</option>
                             <option v-for="option in clientTypes" :key="option.value" :value="option.value">{{
                                 option.label }}</option>
@@ -64,7 +64,7 @@
                     <div>
                         <label for="reclamationType" class="block text-subtitle">TIPO DE RECLAMACIÓN</label>
                         <select id="reclamationType" v-model="formData.reclamationType"
-                            class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md shadow-sm sm:max-w-[50%]">
+                            class="mt-1 block w-full pl-3 pr-10 py-2 border text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md shadow-sm sm:max-w-[50%]">
                             <option value="" disabled>Seleccione</option>
                             <option v-for="option in reclamationTypes" :key="option.value" :value="option.value">{{
                                 option.label }}</option>
@@ -182,7 +182,7 @@
                             <label for="bien"
                                 class="w-full sm:w-1/3 text-sm font-medium text-gray-700 mb-1 sm:mb-0">Bien</label>
                             <select id="bien" v-model="formData.bien"
-                                class="w-full sm:w-2/3 pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md shadow-sm">
+                                class="w-full border sm:w-2/3 pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm rounded-md shadow-sm">
                                 <option value="" disabled>Producto</option>
                                 <option v-for="option in bienes" :key="option.value" :value="option.value">{{
                                     option.label }}</option>
